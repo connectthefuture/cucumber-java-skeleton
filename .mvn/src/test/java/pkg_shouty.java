fd909c8df1c1c4b59a8ddef6d3b579c9aa58e8b1
connectthefuture <connectthefuture@tuta.io>
2020-05-21 08:07:07 -0500

    cucumber-school-test1
    first school example

---
+++ .mvn/src/test/java/pkg_shouty.java
@@ -0,0 +1,29 @@
+public class pkg_shouty {
+package shouty;
+
+    public class StepDefinitions {
+        @Given("Lucy is {int} metres from Sean")
+        public void lucy_is_metres_from_Sean(Integer int1) {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+
+        Step undefined
+        You can implement missing steps with the snippets below:
+        @When("Sean shouts {string}")
+        public void sean_shouts(String string) {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+
+        Step undefined
+        You can implement missing steps with the snippets below:
+        @Then("Lucy should hear Sean's message")
+        public void lucy_should_hear_Sean_s_message() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+    }
+}
